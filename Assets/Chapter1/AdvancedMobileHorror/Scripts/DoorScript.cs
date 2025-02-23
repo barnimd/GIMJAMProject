9cda71622a1a3fd1dddb05310628466e4c96c271
Baraaaa <114379571+barnimd@users.noreply.github.com>
2025-02-23 20:45:58 +0700

    UP

--- Assets/Chapter1/AdvancedMobileHorror/Scripts/DoorScript.cs
+++ Assets/Chapter1/AdvancedMobileHorror/Scripts/DoorScript.cs
@@ -6,15 +6,13 @@ namespace AdvancedHorrorFPS
 {
     public class DoorScript : MonoBehaviour
     {
-        public bool isLocked = false;
+        public bool isLocked = true;
         public bool giveLockMessage = false;
         public bool isOpened = false;
         public int KeyID_ToOpen = 0;
         public float AngleForOpening = 60;
         private Animation animation;
         public bool leftDoor = false;
-
-        public bool isMainDoor = false; // Tambahan: Apakah ini pintu utama?
 
         private void Start()
         {
@@ -28,6 +26,11 @@ namespace AdvancedHorrorFPS
             TryToOpen();
         }
 
+        public void UnlockedDoor()
+        {
+            isLocked = false;
+        }
+
         public void UnlockAndOpen()
         {
             isLocked = false;
@@ -37,23 +40,12 @@ namespace AdvancedHorrorFPS
         private float LastTimeTry = 0;
         public void TryToOpen()
         {
-            if (isMainDoor) // Jika ini pintu utama, cek hint dulu
-            {
-                if (!HintManager.Instance.AreAllHintsActive()) // Jika semua hint belum aktif
-                {
-                    Debug.Log("Pintu utama masih terkunci! Aktifkan semua hint dulu.");
-                    GameCanvas.Instance.Show_Warning("The main door is locked. Find the right path!");
-                    AudioManager.Instance.Play_Door_TryOpen();
-                    return;
-                }
-            }
-
             if (Time.time > LastTimeTry + 1)
             {
                 LastTimeTry = Time.time;
                 if (isLocked)
                 {
-                    if (HeroPlayerScript.Instance.Hand_Key.activeSelf)
+                    if(HeroPlayerScript.Instance.Hand_Key.activeSelf)
                     {
                         if (HeroPlayerScript.Instance.GetCurrentKey() == KeyID_ToOpen)
                         {
@@ -61,11 +53,7 @@ namespace AdvancedHorrorFPS
                             HeroPlayerScript.Instance.Hand_Key.SetActive(false);
                             InventoryManager.Instance.Remove_Item();
                             AudioManager.Instance.Play_Door_UnLock();
-                            GameObject nextDoor = Physics.OverlapSphere(transform.position, 4)
-                                .Where(x => x.GetComponent<DoorScript>() != null)
-                                .Where(x => x.transform != transform)
-                                .Select(x => x.gameObject)
-                                .FirstOrDefault();
+                            GameObject nextDoor = Physics.OverlapSphere(transform.position, 4).Where(x => x.GetComponent<DoorScript>() != null).Where(x => x.transform != transform).Select(x => x.gameObject).FirstOrDefault();
                             if (nextDoor != null)
                             {
                                 nextDoor.GetComponent<DoorScript>().UnlockAndOpen();
@@ -74,7 +62,18 @@ namespace AdvancedHorrorFPS
                         }
                         else
                         {
-                            PlayTryOpenAnimation();
+                            if (leftDoor)
+                            {
+                                animation["Custom_Animation_DoorLeft_Try"].time = 0;
+                                animation["Custom_Animation_DoorLeft_Try"].speed = 1;
+                                animation.Play("Custom_Animation_DoorLeft_Try");
+                            }
+                            else
+                            {
+                                animation["Custom_Animation_DoorRight_Try"].time = 0;
+                                animation["Custom_Animation_DoorRight_Try"].speed = 1;
+                                animation.Play("Custom_Animation_DoorRight_Try");
+                            }
                             AudioManager.Instance.Play_Door_TryOpen();
                             if (giveLockMessage)
                             {
@@ -85,16 +84,23 @@ namespace AdvancedHorrorFPS
                     }
                     else
                     {
-                        GameObject nextDoor = Physics.OverlapSphere(transform.position, 4)
-                            .Where(x => x.GetComponent<DoorScript>() != null)
-                            .Where(x => x.transform != transform)
-                            .Select(x => x.gameObject)
-                            .FirstOrDefault();
+                        GameObject nextDoor = Physics.OverlapSphere(transform.position, 4).Where(x => x.GetComponent<DoorScript>() != null).Where(x => x.transform != transform).Select(x => x.gameObject).FirstOrDefault();
                         if (nextDoor != null)
                         {
                             nextDoor.GetComponent<DoorScript>().TryToOpen();
                         }
-                        PlayTryOpenAnimation();
+                        if (leftDoor)
+                        {
+                            animation["Custom_Animation_DoorLeft_Try"].time = 0;
+                            animation["Custom_Animation_DoorLeft_Try"].speed = 1;
+                            animation.Play("Custom_Animation_DoorLeft_Try");
+                        }
+                        else
+                        {
+                            animation["Custom_Animation_DoorRight_Try"].time = 0;
+                            animation["Custom_Animation_DoorRight_Try"].speed = 1;
+                            animation.Play("Custom_Animation_DoorRight_Try");
+                        }
                         AudioManager.Instance.Play_Door_TryOpen();
                         if (giveLockMessage)
                         {
@@ -105,25 +111,30 @@ namespace AdvancedHorrorFPS
                 }
                 else
                 {
-                    GameObject nextDoor = Physics.OverlapSphere(transform.position, 4)
-                        .Where(x => x.GetComponent<DoorScript>() != null)
-                        .Where(x => x.transform != transform)
-                        .Select(x => x.gameObject)
-                        .FirstOrDefault();
+                    GameObject nextDoor = Physics.OverlapSphere(transform.position, 4).Where(x => x.GetComponent<DoorScript>() != null).Where(x => x.transform != transform).Select(x => x.gameObject).FirstOrDefault();
                     if (nextDoor != null)
                     {
                         nextDoor.GetComponent<DoorScript>().TryToOpen();
                     }
-
                     BlinkEffect effect = GetComponent<BlinkEffect>();
                     if (effect != null && effect.enabled)
                     {
                         effect.Disable();
                     }
-
-                    if (!isOpened)
+                    if (isOpened == false)
                     {
-                        PlayOpenAnimation();
+                        if(leftDoor)
+                        {
+                            animation["Custom_Animation_DoorLeft_Open"].time = 0;
+                            animation["Custom_Animation_DoorLeft_Open"].speed = 1;
+                            animation.Play("Custom_Animation_DoorLeft_Open");
+                        }
+                        else
+                        {
+                            animation["Custom_Animation_DoorRight_Open"].time = 0;
+                            animation["Custom_Animation_DoorRight_Open"].speed = 1;
+                            animation.Play("Custom_Animation_DoorRight_Open");
+                        }
                         isOpened = true;
                         AudioManager.Instance.Play_Door_Wooden_Open();
                     }
@@ -131,57 +142,20 @@ namespace AdvancedHorrorFPS
                     {
                         isOpened = false;
                         AudioManager.Instance.Play_Door_Close();
-                        PlayCloseAnimation();
+                        if (leftDoor)
+                        {
+                            animation["Custom_Animation_DoorLeft_Open"].time = animation["Custom_Animation_DoorLeft_Open"].length;
+                            animation["Custom_Animation_DoorLeft_Open"].speed = -1;
+                            animation.Play("Custom_Animation_DoorLeft_Open");
+                        }
+                        else
+                        {
+                            animation["Custom_Animation_DoorRight_Open"].time = animation["Custom_Animation_DoorRight_Open"].length;
+                            animation["Custom_Animation_DoorRight_Open"].speed = -1;
+                            animation.Play("Custom_Animation_DoorRight_Open");
+                        }
                     }
                 }
-            }
-        }
-
-        private void PlayTryOpenAnimation()
-        {
-            if (leftDoor)
-            {
-                animation["Custom_Animation_DoorLeft_Try"].time = 0;
-                animation["Custom_Animation_DoorLeft_Try"].speed = 1;
-                animation.Play("Custom_Animation_DoorLeft_Try");
-            }
-            else
-            {
-                animation["Custom_Animation_DoorRight_Try"].time = 0;
-                animation["Custom_Animation_DoorRight_Try"].speed = 1;
-                animation.Play("Custom_Animation_DoorRight_Try");
-            }
-        }
-
-        private void PlayOpenAnimation()
-        {
-            if (leftDoor)
-            {
-                animation["Custom_Animation_DoorLeft_Open"].time = 0;
-                animation["Custom_Animation_DoorLeft_Open"].speed = 1;
-                animation.Play("Custom_Animation_DoorLeft_Open");
-            }
-            else
-            {
-                animation["Custom_Animation_DoorRight_Open"].time = 0;
-                animation["Custom_Animation_DoorRight_Open"].speed = 1;
-                animation.Play("Custom_Animation_DoorRight_Open");
-            }
-        }
-
-        private void PlayCloseAnimation()
-        {
-            if (leftDoor)
-            {
-                animation["Custom_Animation_DoorLeft_Open"].time = animation["Custom_Animation_DoorLeft_Open"].length;
-                animation["Custom_Animation_DoorLeft_Open"].speed = -1;
-                animation.Play("Custom_Animation_DoorLeft_Open");
-            }
-            else
-            {
-                animation["Custom_Animation_DoorRight_Open"].time = animation["Custom_Animation_DoorRight_Open"].length;
-                animation["Custom_Animation_DoorRight_Open"].speed = -1;
-                animation.Play("Custom_Animation_DoorRight_Open");
             }
         }
     }
